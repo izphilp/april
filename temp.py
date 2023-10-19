3d5a79e60411800550920b36d398c75d757675a8
izphilp <philbaloa2@gmail.com>
2023-10-19 15:52:42 -0600

    step1

---
+++ temp.py
@@ -0,0 +1,6 @@
+"""Temperature in degrees Fahrenheit (*F) = (Temperature in degrees Celcius (*C) * 9/5) + 32"""
+
+"Convert temp from celcius to fahrenheit"
+degrees = 25
+convertToFahrenheit = (25* 9/5) + 32
+print(f"{degrees} degrees is equal to {convertToFahrenheit:.0f} fahrenheit")
